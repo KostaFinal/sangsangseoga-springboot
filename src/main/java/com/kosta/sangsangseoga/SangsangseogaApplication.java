d4aaa6ab65958cbf919297863f3dceb750c9b4bb
KJY0618 <98rmmr@gmail.com>
2026-06-26 16:08:23 +0900

    first commit

---
+++ src/main/java/com/kosta/sangsangseoga/SangsangseogaApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.sangsangseoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangsangseogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangsangseogaApplication.class, args);
+	}
+
+}
