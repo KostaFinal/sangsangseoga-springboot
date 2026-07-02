6da655871fa087bdb5131785d97c1c08f3c8dac9
ChoiSW <seungwoo714@gmail.com>
2026-07-02 18:17:32 +0900

    fix: WeeklyBookRanking 리포지토리·테이블명 정정
    WeeklyBookStatRepository를 실제 매핑 엔티티 이름에 맞춰 WeeklyBookRankingRepository로
    변경한다.

--- src/main/java/com/kosta/sangsangseoga/domain/admin/repository/WeeklyBookRankingRepository.java
+++ src/main/java/com/kosta/sangsangseoga/domain/admin/repository/WeeklyBookRankingRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kosta.sangsangseoga.domain.book.entity.WeeklyBookRanking;
 
-public interface WeeklyBookStatRepository extends JpaRepository<WeeklyBookRanking, Long> {
+public interface WeeklyBookRankingRepository extends JpaRepository<WeeklyBookRanking, Long> {
 }
