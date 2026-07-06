464286de4cefe7741677c32e7313f42bde45f437
ChoiSW <seungwoo714@gmail.com>
2026-07-06 18:00:47 +0900

    fix: BookType에서 NONFICTION 제거 및 에러 메시지 동기화
    ERD의 book_type ENUM 값(ESSAY/POEM/FAIRY_TALE/NOVEL)과 실제 코드가
    NONFICTION 유무로 어긋나 있던 걸 맞춤. INVALID_BOOK_TYPE 에러 메시지도 함께 수정.

--- src/main/java/com/kosta/sangsangseoga/domain/book/exception/BookErrorCode.java
+++ src/main/java/com/kosta/sangsangseoga/domain/book/exception/BookErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum BookErrorCode implements ErrorCode {
 
-    INVALID_BOOK_TYPE(HttpStatus.BAD_REQUEST, "bookType 값이 올바르지 않습니다 (ESSAY/POEM/NONFICTION/FAIRY_TALE/NOVEL 중 하나여야 합니다)"),
+    INVALID_BOOK_TYPE(HttpStatus.BAD_REQUEST, "bookType 값이 올바르지 않습니다 (ESSAY/POEM/FAIRY_TALE/NOVEL 중 하나여야 합니다)"),
     INVALID_SORT(HttpStatus.BAD_REQUEST, "sort 값이 올바르지 않습니다 (latest/popular/likes 중 하나여야 합니다)");
 
     private final HttpStatus status;
