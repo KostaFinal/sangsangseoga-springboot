ed49ff35c6b253a0a586ffc50deb232449dace93
ChoiSW <seungwoo714@gmail.com>
2026-07-08 18:50:43 +0900

    chore: 미사용 editor 도메인 스텁 삭제

--- src/main/java/com/kosta/sangsangseoga/domain/editor/repository/PoemSettingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kosta.sangsangseoga.domain.editor.repository;
-
-import com.kosta.sangsangseoga.domain.editor.entity.PoemSetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PoemSettingRepository extends JpaRepository<PoemSetting, Long> {
-}
