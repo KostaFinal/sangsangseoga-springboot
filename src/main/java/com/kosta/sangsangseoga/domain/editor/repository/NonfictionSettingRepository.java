972f6b521cf3cb65444b909eb3cc4e737945445f
ChoiSW <84121891+ChoiSWgg@users.noreply.github.com>
2026-07-08 18:51:40 +0900

    chore: 미사용 editor 도메인 스텁 삭제 (#40)

--- src/main/java/com/kosta/sangsangseoga/domain/editor/repository/NonfictionSettingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kosta.sangsangseoga.domain.editor.repository;
-
-import com.kosta.sangsangseoga.domain.editor.entity.NonfictionSetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NonfictionSettingRepository extends JpaRepository<NonfictionSetting, Long> {
-}
